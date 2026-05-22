ad3e34a65455dc993f13e3a26cf7e9f9b9f36746
transitionteks <transitionteks@gmail.com>
2026-05-21 19:35:00 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Transition Teks Website
+
+Open `index.html` in your browser to preview.
+
+Upload this folder to GitHub, then connect it to Cloudflare Pages.
---
+++ script.js
@@ -0,0 +1,4 @@
+const menuButton = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+menuButton.addEventListener('click', () => nav.classList.toggle('open'));
+document.querySelectorAll('.nav a').forEach((link) => link.addEventListener('click', () => nav.classList.remove('open')));
